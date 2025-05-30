e647fd558d90221858b61c004f3cb5da57d51862
Nikhil S K <nikhilseb23@gmail.com>
2025-05-30 08:51:57 +0530

    Clean up default React app and add styled header

---
+++ hackhunt-frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <header className="app-header">
+        <h1>🚀 HackHunt</h1>
+        <p>Find and register for top tech hackathons in India</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
